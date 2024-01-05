00b0c3a63970fd26942848f444adc63cc1e2c5a4
Eliel <125eramirez@frhsd.com>
2024-01-05 10:11:31 -0500

    1

--- js/games.js
+++ js/games.js
@@ -22,10 +22,66 @@ function intialize(){
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
-            tile.innerText = 'P';
+            tile.innerText = '';
             document.getElementById("board").appendChild(tile);
 
         }
     }
 
+    // Listen for Key Press
+    document.addEventListener("keyup" , (e) =>{
+        if (gameOver) return;
+
+        // alert(e.code);
+    // alert(e.code);
+    if("KeyA" <= e.code && e.code <= "KeyZ"){
+        if(col < width){
+            let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+            if(currentTile.innerText == ""){
+            currentTile.innerText = e.code[3]
+                col++;
+        }
+        }
+    }
+else if (e.code =="Backspace"){
+    if (0 < col && col <= width){
+        col--;
+    }
+    let currentTile = document.getElementById(row.toString()+ "-" + col.toString());
+    currentTile.innerText = "";
+}
+else if(e.code == "Enter"){
+    update();
+    row += 1; //start new row
+    col = 0; //start at 0 for new row
+}
+
+if (!gameOver && row == height){
+    gameOver = true;
+    document.getElementById("answer").innerText = word;
+}
+    })
+} 
+
+function update() {
+    let correct = 0;
+    for (let c = 0; c < width; c++){
+        let currentTile = document.getElementById(row.toString()+ "-" + c);
+        let letter = currTile.innerText;
+        
+        //Is it in the correct positiion?
+        if (word[c] == letter) {
+            title.classList.add("correct");
+            correct += 1;
+        } // Is it in the word?
+        else if (word.includes(letter)){
+            tile.classList.add("present");
+        }// Not in the world
+        else {
+            tile.classList.add("absent");
+        }
+        if (correct == width) {
+            gameOver = true;
+        }
+    }
 }
